847675c4e3566256c42abefff78630fcd58ad4ec
Revsky <angel1vengador100@gmail.com>
2022-02-07 10:59:28 -0600

    Creando overlay para la imagen

--- src/js/app.js
+++ src/js/app.js
@@ -17,6 +17,28 @@ function crearGaleria(){
             <img loading="lazy" alt="imagen ${i}" src="build/img/thumb/${i}.jpg">
         `;
 
+        imagen.onclick = function(){
+            mostrarImagen(i);
+        }
+
         galeria.appendChild(imagen);
     }
+}
+
+function mostrarImagen( index ){
+    console.log(`Mostrando.. ${index}`);
+
+    const imagen = document.createElement('picture')
+    imagen.innerHTML = `
+        <source srcet="build/img/grande/${index}.avif" type="image/avif">
+        <source srcet="build/img/grande/${index}.webp" type="image/webp">
+        <img loading="lazy" alt="imagen ${index}" src="build/img/grande/${index}.jpg">
+    `;
+
+    const overlay = document.createElement('DIV')
+    overlay.appendChild(imagen)
+    overlay.classList.add('overlay');
+
+    const body = document.querySelector('body');
+    body.appendChild(overlay)
 }
